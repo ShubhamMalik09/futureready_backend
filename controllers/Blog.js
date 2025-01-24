8e6b80096510a9a99e9aaa1a1902e070e35c54a0
Shubham Malik <shubhammalik.sm05@gmail.com>
2025-01-24 10:52:39 +0530

    added routes for update, delete and getAll

--- controllers/Blog.js
+++ controllers/Blog.js
@@ -48,3 +48,109 @@ exports.createBlog = async (req,res) =>{
         })
       }
 }
+
+exports.updateBlog = async (req,res) =>{
+    try{
+        // const userId = req.user.id;
+        let {
+            id,
+            title,
+            description,
+            category,
+        } = req.body;
+
+        if(!title || !description || !category ){
+            return res.status(400).json({
+                success: false,
+                message: "All Fields are Mandatory",
+            })
+        }
+
+        // const user = await User.findById(userId)
+        // if(!user){
+        //     return res.status(404).json({
+        //         success: false,
+        //         message: "User Details Not Found",
+        //     })
+        // }
+        const updatedBlog = await Blog.findByIdAndUpdate(id,{
+            title:title,
+            description:description,
+            category:category,
+        },{new:true})
+
+        if(!updatedBlog){
+            res.status(400).json({
+                success: false,
+                message: "Blog not found",
+            })
+        }
+
+        // user.blogs.push(newBlog._id);
+        // await user.save();
+
+        res.status(200).json({
+            success: true,
+            data: updatedBlog,
+            message: "Blog Updated Successfully",
+        })
+    }
+    catch (error) {
+        console.error(error)
+        res.status(500).json({
+          success: false,
+          message: "Failed to update blog",
+          error: error.message,
+        })
+      }
+}
+
+exports.deleteBlog = async (req,res) =>{
+    try{
+        // const userId = req.user.id;
+        let { id} = req.body;
+
+        const deletedBlog = await Blog.findByIdAndDelete(id);
+
+        if(!deletedBlog){
+            res.status(400).json({
+                success: false,
+                message: "Blog not found",
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: deletedBlog,
+            message: "Blog deleted Successfully",
+        })
+    }
+    catch (error) {
+        console.error(error)
+        res.status(500).json({
+          success: false,
+          message: "Failed to delete blog",
+          error: error.message,
+        })
+    }
+}
+
+exports.getAllBlog = async(req,res)=>{
+    try{
+        const blogs = await Blog.find();
+
+        res.status(200).json({
+            success: true,
+            data: blogs,
+            message: "All Blogs get Successfully",
+        })
+    }
+    catch (error) {
+        console.error(error)
+        res.status(500).json({
+          success: false,
+          message: "Failed to get all blogs",
+          error: error.message,
+        })
+    }
+}
--- routes/Blog.js
+++ routes/Blog.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {createBlog} = require("../controllers/Blog")
+const {createBlog, updateBlog, deleteBlog, getAllBlog} = require("../controllers/Blog")
 
 router.post("/create",createBlog);
+router.put('/update',updateBlog);
+router.delete('/delete',deleteBlog);
+router.get('/blogs',getAllBlog)
 
 module.exports = router;
